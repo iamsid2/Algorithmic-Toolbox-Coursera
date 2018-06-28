ce056c355bd4d3e8ebb2971de7618b7b2bee9fbb
iamsid2 <shaktimund97@gmail.com>
2018-06-28 23:29:25 +0530

    for the day 2 ninja technique

--- week4/majority_element.cpp
+++ week4/majority_element.cpp
@@ -28,7 +28,9 @@ int BSearch(int a[], int low, int high, int x)
 int get_majority_element(int a, int left, int right)
 {
   if (left == right)
+  {
     return -1;
+  }
   if (left + 1 == right)
     return a[left];
 
