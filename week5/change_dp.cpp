e1c629cff9d2046b444be95f292c8d460c06e094
iamsid2 <shaktimund97@gmail.com>
2018-07-06 23:34:20 +0530

    Week__5

--- week5/change_dp.cpp
+++ week5/change_dp.cpp
@@ -1,12 +1,16 @@
 #include <iostream>
 
-int get_change(int m) {
+using namespace std;
+
+int get_change(int m)
+{
   //write your code here
   return m / 4;
 }
 
-int main() {
+int main()
+{
   int m;
-  std::cin >> m;
-  std::cout << get_change(m) << '\n';
+  cin >> m;
+  cout << get_change(m) << '\n';
 }
