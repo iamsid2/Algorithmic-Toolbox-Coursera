56806d7ab7fe6f5045cc8e34e4e94d48b2a85b21
iamsid2 <shaktimund97@gmail.com>
2018-07-10 00:26:23 +0530

    Change_dp

--- week5/change_dp.cpp
+++ week5/change_dp.cpp
@@ -1,11 +1,33 @@
 #include <iostream>
+#include<algorithm>
+#include<climits>
 
 using namespace std;
 
-int get_change(int m)
+int get_change(int money)
 {
+    int coins[3] = {1,3,4};
+    int i,j,MinCoins[money],NCoins;
+    MinCoins[0] = 0;
+    for(i=1;i<=money;i++)
+    {
+        MinCoins[i] = INT_MAX;
+    }
 
-  return m ;
+    for(i=1;i<=money;i++)
+    {
+        for(j=0;j<3;j++)
+        {
+            if(i >= coins[j])
+            {
+                NCoins = MinCoins[i-coins[j]] + 1;
+                if(NCoins < MinCoins[i])
+                    MinCoins[i] = NCoins;
+            }
+        }
+    }
+    return MinCoins[money];
+
 }
 
 int main()
